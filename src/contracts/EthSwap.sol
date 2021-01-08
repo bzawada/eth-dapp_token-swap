1bd41af765101de8ca03b181145d2ddc5a81cac7
Bartek Zawada <krucjo12@gmail.com>
2021-01-08 02:01:07 +0100

    feat: add ethSwap contract

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,7 +1,46 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.5;
+
+import "./Token.sol";
 
 contract EthSwap {
-    function EthSwap(){
+    string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint public rate = 100;
 
+    event TokensPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    event TokensSold(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    );
+
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+
+        require(token.balanceOf(address(this)) >= tokenAmount);
+        emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint _amount) public {
+        require(token.balanceOf(msg.sender) >= _amount);
+        uint etherAmount = _amount / rate;
+        require(address(this).balance >= etherAmount);
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        emit TokensSold(msg.sender, address(token), _amount, rate);
     }
 }
